1604af882c5eb6e133f0047ef5185cd7da3a3e2e
musegi <marianna.reznik.00@gmail.com>
2025-01-23 14:27:35 +0300

    Тз 15: add bookings

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -15,6 +15,7 @@ import lombok.Setter;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
     @Column(nullable = false, length = 255)
     private String name;
